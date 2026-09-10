f9fdb43aa4c5137ccd1f680f022e6c51741a1a88
praveenkumar-666 <nagarajanpraveen4@gmail.com>
2026-09-10 13:43:51 +0530

    mae the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -72,8 +72,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         productId : productId,
         quantity : 1
     })}
-    
-    
-    console.log(cart)
+
+    let cartQuantity = 0
+
+    cart.forEach((items)=>{
+      cartQuantity += items.quantity
+    })
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+
   })
 })
