53fe79bd6cdfd2c98257e34b9bd4643de1b502a3
praveenkumar-666 <nagarajanpraveen4@gmail.com>
2026-09-10 16:24:34 +0530

    improve the quantity selector option

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=> {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -60,17 +60,19 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',()=>{
     const productId = button.dataset.productId
     let matchingItem 
+    let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
+    let quantityValue = Number(quantitySelector.value)
     cart.forEach((item)=>{
       if(productId === item.productId){
         matchingItem = item
       }
     })
       if(matchingItem){
-        matchingItem.quantity += 1
+        matchingItem.quantity += quantityValue
       }else{
         cart.push({
         productId : productId,
-        quantity : 1
+        quantity : quantityValue
     })}
 
     let cartQuantity = 0
@@ -79,6 +81,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
       cartQuantity += items.quantity
     })
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+    
+   
 
   })
 })
