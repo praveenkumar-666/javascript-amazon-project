a9b22b0a0056227deb412987e9e82a5d547ecf1c
praveenkumar-666 <nagarajanpraveen4@gmail.com>
2026-09-24 11:37:25 +0530

    implement quantity update functionality and styling in checkout page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -36,11 +36,15 @@ cart.forEach((cartItems) =>{
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">${cartItems.quantity}</span>
+                    Quantity: <span class="quantity-label js-quantity-label">${cartItems.quantity}</span>
                   </span>
-                  <span class="update-quantity-link link-primary">
+                  <span class="update-quantity-link link-primary js-update-btn" data-product-id = "${matchingProduct.id}">
                     Update
                   </span>
+                  <input class="quantity-input js-quantity-input-${matchingProduct.id}"></input>
+
+                  <span class="save-quantity-link js-save-btn"  data-product-id = "${matchingProduct.id}">Save</span>
+
                   <span class="delete-quantity-link link-primary js-delete-link" data-product-id ="${matchingProduct.id}">
                     Delete
                   </span>
@@ -119,5 +123,42 @@ function updateCartQuantity(){
 
 }
 updateCartQuantity()
+
+const updateBtn = document.querySelectorAll('.js-update-btn')
+
+updateBtn.forEach((link)=>{
+
+  link.addEventListener('click',()=>{
+    const productId = link.dataset.productId
+    console.log(productId)
+    
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`)
+    container.classList.add('is-editing-quantity')
+
+    
+  })
+
+})
+
+const saveBtn = document.querySelectorAll('.js-save-btn')
+
+saveBtn.forEach((link)=>{
+  link.addEventListener('click',()=>{
+  const productId = link.dataset.productId
+  const container = document.querySelector(`.js-cart-item-container-${productId}`)
+  container.classList.remove('is-editing-quantity')
+
+  const quantityInput = document.querySelector(`.js-quantity-input-${productId}`)
+  const newQuantity = Number(quantityInput.value)
+
+  console.log(newQuantity)
+
+  })
+})
+
+
+
+
 
     
