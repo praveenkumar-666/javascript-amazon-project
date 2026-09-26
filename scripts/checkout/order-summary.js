5708301d01e4bd3275d578d9ad49df3345200ddb
praveenkumar-666 <nagarajanpraveen4@gmail.com>
2026-09-26 15:06:48 +0530

    implement payment summary rendering and refactor order summary logic

--- scripts/checkout/order-summary.js
+++ scripts/checkout/order-summary.js
@@ -1,7 +1,7 @@
 import { cart , removeFromCart,calculateCartQuantity,updateQuantity, updateDeliveryOPtion } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products,getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
   
 export function renderOrderSummary(){
@@ -12,23 +12,12 @@ export function renderOrderSummary(){
 
           const productId = cartItem.productId
 
-          let matchingProduct 
-          products.forEach((product)=>{
-              if(product.id === productId){
-                  matchingProduct = product
-              }
-
-          })
+          const matchingProduct = getProduct(productId)
           const deliveryOptionId = cartItem.deliveryOptionId ?? cartItem.deliveryDateId
 
-          let deliveryOption 
+          const deliveryOption = getDeliveryOption(deliveryOptionId)
 
-          deliveryOptions.forEach((option)=>{
-            if(option.id === deliveryOptionId){
-              deliveryOption = option
-            }
-          
-          })
+
         
           const today = dayjs()
 
